28dc5719c4a82e400f45bb50c2dfa3b16e005734
Hexiaobin <919760638@qq.com>
2022-12-13 18:06:22 +0800

    改了一部分东西，新增了个chatOpenAI玩具

---
+++ Front/src/components/Index.vue
@@ -0,0 +1,42 @@
+<template>
+<!--这个文件作为网站路由，第一个tab是便捷复制，第二个tab是chatAI-->
+  <div class="index">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="便捷复制" name="first">
+        <easy-copy></easy-copy>
+      </el-tab-pane>
+      <el-tab-pane label="OpenChatAI" name="second">
+        <chat-a-i></chat-a-i>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import easyCopy from "./EasyCopy";
+import chatAI from "./ChatAI";
+export default {
+  name: "index",
+  components: {
+    easyCopy,
+    chatAI
+  },
+  data() {
+    return {
+      activeName: "second"
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.index {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- Front/vue.config.js
+++ Front/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-const serverPath = 'http://localhost:9197' // 开发环境
-// const serverPath = 'http://47.94.164.14:9197' // 正式环境
+const serverPath = 'http://localhost:8081' // 开发环境
+// const serverPath = 'http://47.94.164.14:8081' // 正式环境
 module.exports = defineConfig({
   transpileDependencies: true,
     lintOnSave: false,
